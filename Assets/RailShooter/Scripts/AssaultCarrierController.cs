7014481833607119e9f676cc21693f9125bfdb23
Jonathan Walker <jwsmasher@gmail.com>
2021-07-16 14:37:42 -0500

    Added more buildings and enemies to the first level

--- Assets/RailShooter/Scripts/AssaultCarrierController.cs
+++ Assets/RailShooter/Scripts/AssaultCarrierController.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class AssaultCarrierController : MonoBehaviour
 {
     Rigidbody body;
-    float forwardSpeed;
+    Animator anim;
+    float lastAttackTime = 0;
+    //float forwardSpeed;
+
+    bool[] doorOpen = new bool[2];
 
     void Awake()
     {
         body = GetComponent<Rigidbody>();
-        forwardSpeed = PlayerController.player.forwardpeed;
+        anim = GetComponent<Animator>();
+        doorOpen[0] = false;
+        doorOpen[1] = false;
+        //forwardSpeed = PlayerController.player.forwardpeed;
         
     }
     void Start()
     {
+        StartCoroutine(StartBossBattle());
     }
 
     void Update()
     {
-        
+        if (lastAttackTime >= 5)
+        {
+            ToggleDoorOpen(1);
+            lastAttackTime = 0;
+        }
+        lastAttackTime += Time.deltaTime;
     }
 
     void FixedUpdate()
@@ -28,4 +41,24 @@ public class AssaultCarrierController : MonoBehaviour
         //body.AddForce(movement, ForceMode.VelocityChange);
         body.velocity = movement;
     }
+
+    void ToggleDoorOpen(int doorIndex)
+    {
+        if (doorOpen[doorIndex])
+        {
+            anim.Play("CloseHangar" + doorIndex.ToString());
+        }
+        else
+        {
+            anim.Play("OpenHangar" + doorIndex.ToString());
+        }
+    }
+
+    IEnumerator StartBossBattle()
+    {
+        yield return new WaitForSeconds(5);
+        anim.Play("facePlayer");
+        yield return new WaitForSeconds(2);
+    }
+
 }
